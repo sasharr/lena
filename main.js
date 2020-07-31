a89a5145c7f0886ff460e85562b4e981b045801d
Sasha Lytvinenko <sasha.lytvinenko@ab-soft.net>
2020-07-31 23:10:38 +0300

    popup

--- main.js
+++ main.js
@@ -1,14 +1,31 @@
-$("a").on('click', function (event) {
-    if (this.hash !== "") {
-        event.preventDefault();
+$(window).on('load', function () {
+    $("a").on('click', function (event) {
+        if (this.hash !== "") {
+            event.preventDefault();
 
-        var hash = this.hash;
+            var hash = this.hash;
 
-        $('html, body').animate({
-            scrollTop: $(hash).offset().top
-        }, 800, function () {
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function () {
 
-            window.location.hash = hash;
-        });
-    }
+                window.location.hash = hash;
+            });
+        }
+    });
+
+    $("#popup-open").on("click touch", function () {
+        // event.preventDefault();
+        $('.popup').css("display", "block")
+    });
+
+    $('#popup-close').on("click touch", function () {
+        $('.popup').hide();
+    });
+
+    $(".popup").on("click touch", function (event) {
+        if ($(event.target).closest(".popup-content").length) return;
+
+        $('.popup').hide();
+    });
 });
